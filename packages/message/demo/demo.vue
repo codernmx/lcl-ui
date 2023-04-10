4ca8f21e0a786163051a8bcc97d8c1652084b2ce
codernmx <2507128400@qq.com>
2023-04-11 00:04:39 +0800

    fix:message

---
+++ packages/message/demo/demo.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Message from '../index'
+import { onMounted } from "vue";
+const openMessage = (type) => {
+    Message({ type, str: `this is a ${type} message !` })
+}
+
+</script>
+
+<template>
+    <div style="display: flex;justify-content: space-between;">
+        <lcl-button type="success" @click="openMessage('success')">success消息</lcl-button>
+        <lcl-button type="warning" @click="openMessage('warning')">warning消息</lcl-button>
+        <lcl-button type="success" @click="openMessage('error')">error消息</lcl-button>
+        <lcl-button type="success" @click="openMessage('info')">info消息</lcl-button>
+    </div>
+</template>
+
+<style scoped></style>
